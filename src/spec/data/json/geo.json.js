0a67c7266f9b880f5f122aa3b2c29ca6f795432b
Tom Adams <tomjadams@gmail.com>
2009-04-18 14:59:05 +1000

    stuff

---
+++ src/spec/data/json/geo.json.js
@@ -0,0 +1,83 @@
+{
+  "name": "-27.52553,153.065475",
+  "Status": {
+    "code": 200,
+    "request": "geocode"
+  },
+  "Placemark": [ {
+    "id": "p1",
+    "address": "35 Boronia Ave, Holland Park West QLD 4121, Australia",
+    "AddressDetails": {"Country": {"CountryNameCode": "AU","CountryName": "Australia","AdministrativeArea": {"AdministrativeAreaName": "QLD","Locality": {"LocalityName": "Holland Park West","Thoroughfare":{"ThoroughfareName": "35 Boronia Ave"},"PostalCode": {"PostalCodeNumber": "4121"}}}},"Accuracy": 8},
+    "ExtendedData": {
+      "LatLonBox": {
+        "north": -27.5223824,
+        "south": -27.5286776,
+        "east": 153.0686226,
+        "west": 153.0623274
+      }
+    },
+    "Point": {
+      "coordinates": [ 153.0654750, -27.5255300, 0 ]
+    }
+  }, {
+    "id": "p2",
+    "address": "Holland Park West QLD, Australia",
+    "AddressDetails": {"Country": {"CountryNameCode": "AU","CountryName": "Australia","AdministrativeArea": {"AdministrativeAreaName": "QLD","Locality": {"LocalityName": "Holland Park West"}}},"Accuracy": 4},
+    "ExtendedData": {
+      "LatLonBox": {
+        "north": -27.5135930,
+        "south": -27.5393150,
+        "east": 153.0720450,
+        "west": 153.0502080
+      }
+    },
+    "Point": {
+      "coordinates": [ 153.0612690, -27.5255190, 0 ]
+    }
+  }, {
+    "id": "p3",
+    "address": "4121, Australia",
+    "AddressDetails": {"Country": {"CountryNameCode": "AU","CountryName": "Australia","PostalCode": {"PostalCodeNumber": "4121"}},"Accuracy": 5},
+    "ExtendedData": {
+      "LatLonBox": {
+        "north": -27.5081051,
+        "south": -27.5431057,
+        "east": 153.0854099,
+        "west": 153.0294751
+      }
+    },
+    "Point": {
+      "coordinates": [ 153.0574425, -27.5256054, 0 ]
+    }
+  }, {
+    "id": "p4",
+    "address": "Queensland, Australia",
+    "AddressDetails": {"Country": {"CountryNameCode": "AU","CountryName": "Australia","AdministrativeArea": {"AdministrativeAreaName": "QLD"}},"Accuracy": 2},
+    "ExtendedData": {
+      "LatLonBox": {
+        "north": -10.6875420,
+        "south": -29.1778970,
+        "east": 153.5521710,
+        "west": 137.9959570
+      }
+    },
+    "Point": {
+      "coordinates": [ 145.7740021, -19.7061005, 0 ]
+    }
+  }, {
+    "id": "p5",
+    "address": "Australia",
+    "AddressDetails": {"Country": {"CountryNameCode": "AU","CountryName": "Australia"},"Accuracy": 1},
+    "ExtendedData": {
+      "LatLonBox": {
+        "north": -9.2106000,
+        "south": -44.7779000,
+        "east": 159.2029000,
+        "west": 110.8740000
+      }
+    },
+    "Point": {
+      "coordinates": [ 133.7751360, -25.2743980, 0 ]
+    }
+  } ]
+}
