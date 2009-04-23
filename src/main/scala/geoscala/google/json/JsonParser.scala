29dc6e6a68366d9099da3b722bdea2421762c4a6
Tom Adams <tomjadams@gmail.com>
2009-04-24 09:59:31 +1000

    dodgy json parser

--- src/main/scala/geoscala/google/json/JsonParser.scala
+++ src/main/scala/geoscala/google/json/JsonParser.scala
@@ -1,16 +1,33 @@
 package geoscala.google.json
 
-import net.sf.json.{JSONArray, JSONObject}
+import net.sf.json.{JSONArray, JSONObject}, JSONObject._, JSONArray._
+import scalaz.OptionW, OptionW._
 import types.Placemark
 
 final class JsonParser {
   def placemarks(json: String): Either[Throwable, List[Placemark]] =
     try {
       val result: JSONObject = JSONObject.fromObject(json)
-      val placemarks: JSONArray = result.getJSONArray("Placemark")
+      val maybePlacemarks: Option[JSONArray] = onull(result.optJSONArray("Placemark"))
+
+      maybePlacemarks.map((placemarks: JSONArray) => {
+        val x = placemarks.iterator().foreach(a => a.foreach(p => println(">> p: " + p)))
+        1
+      })
+
+//      maybePlacemarks.map((placemarks: JSONArray) => {
+//        toCollection(placemarks).map((j: JSONObject) => parsePlaceMark(j))
+//      }).toRight[Throwable](exception("Unable to find Placemark array"))
+//
       Right(Nil)
     }
     catch {
       case t: Throwable => Left(t)
     }
+
+  def parsePlaceMark(placemark: JSONObject): Placemark = {
+    error("huzzah")
+  }
+
+  def exception(msg: String) = new Exception(msg)
 }
