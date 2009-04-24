6ab094f113c6556e7fa433a4ba49266f1563e0dd
Tom Adams <tomjadams@gmail.com>
2009-04-24 13:27:30 +1000

    parser infrastructure working

--- src/main/scala/geoscala/google/json/JsonParser.scala
+++ src/main/scala/geoscala/google/json/JsonParser.scala
@@ -1,13 +1,13 @@
 package geoscala.google.json
 
-import net.sf.json.{JSONArray, JSONObject}, JSONObject._, JSONArray._
+import net.sf.json.{JSONArray, JSONObject}, JSONObject._
 import scalaz.OptionW, OptionW._
 import types.Placemark
 
 final class JsonParser {
   def placemarks(json: String): Either[Throwable, List[Placemark]] =
     try {
-      val maybePlacemarks = onull(JSONObject.fromObject(json).optJSONArray("Placemark"))
+      val maybePlacemarks = onull(fromObject(json).optJSONArray("Placemark"))
       val maybeParsedPlacemarks = maybePlacemarks.map(placemarks => 0.until(placemarks.size).map(placemarks.getJSONObject(_)).map(parsePlaceMark(_)).toList)
       maybeParsedPlacemarks.toRight[Throwable](exception("Unable to find Placemark array"))
     }
