1fdfae21ae7d901a0accd9b60353ae1de878efe5
Tom Adams <tomjadams@gmail.com>
2009-04-27 20:31:14 +1000

    parsing placemark

--- src/main/scala/geoscala/google/json/JsonParser.scala
+++ src/main/scala/geoscala/google/json/JsonParser.scala
@@ -5,19 +5,37 @@ import scalaz.OptionW, OptionW._
 import types.Placemark
 
 final class JsonParser {
+  import NullSafeJSONObject._
+
   def placemarks(json: String): Either[Throwable, List[Placemark]] =
     try {
-      val maybePlacemarks = onull(fromObject(json).optJSONArray("Placemark"))
+      val maybePlacemarks = fromObject(json).jsonArray("Placemark")
       val maybeParsedPlacemarks = maybePlacemarks.map(placemarks => 0.until(placemarks.size).map(placemarks.getJSONObject(_)).map(parsePlaceMark(_)).toList)
       maybeParsedPlacemarks.toRight[Throwable](exception("Unable to find Placemark array"))
-    }
-    catch {
+    } catch {
       case t: Throwable => Left(t)
     }
 
   def parsePlaceMark(placemark: JSONObject): Placemark = {
+    println("Parsing placemark: " + placemark)
+
+    //    placemark.optString()
+    //    new Placemark()
+
     error("huzzah")
   }
 
   def exception(msg: String) = new Exception(msg)
+}
+
+private object NullSafeJSONObject {
+  implicit def JSONObjectToNullSafeJSONObject(source: JSONObject): NullSafeJSONObject = new NullSafeJSONObject(source)
+}
+
+private final class NullSafeJSONObject(source: JSONObject) {
+  def jsonArray(key: String): Option[JSONArray] = onull(source.optJSONArray(key))
+
+  def string(key: String): Option[String] = onull(source.optString(key))
+
+  def double(key: String): Option[Double] = onull(source.optDouble(key))
 }
