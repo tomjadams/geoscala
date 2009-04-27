f08edadbd8a7af871451019eab778c8d56c270db
Tom Adams <tomjadams@gmail.com>
2009-04-27 22:02:20 +1000

    Huzzah! Simple case working

--- src/main/scala/geoscala/google/json/JsonParser.scala
+++ src/main/scala/geoscala/google/json/JsonParser.scala
@@ -2,10 +2,10 @@ package geoscala.google.json
 
 import net.sf.json.{JSONArray, JSONObject}, JSONObject._
 import scalaz.OptionW, OptionW._
-import types.Placemark
+import types.{Country, AddressDetails, GeoLocationSearchAccuracy, Placemark}
 
 final class JsonParser {
-  import NullSafeJSONObject._
+  import GeoLocationSearchAccuracy._, NullSafeJSONObject._
 
   def placemarks(json: String): Either[Throwable, List[Placemark]] =
     try {
@@ -16,14 +16,22 @@ final class JsonParser {
       case t: Throwable => Left(t)
     }
 
-  def parsePlaceMark(placemark: JSONObject): Placemark = {
-    println("Parsing placemark: " + placemark)
-
-    //    placemark.optString()
-    //    new Placemark()
-
-    error("huzzah")
+  def parsePlaceMark(placemark: JSONObject) = {
+    val details = parseAddressDetails(placemark.getJSONObject("AddressDetails"))
+    new Placemark(placemark.getString("id"), placemark.getString("address"), details)
   }
+
+  def parseAddressDetails(addressDetails: JSONObject) = {
+    val country = parseCountry(addressDetails.getJSONObject("Country"))
+    new AddressDetails(country, addressDetails.getInt("Accuracy"))
+  }
+
+  def parseCountry(country: JSONObject) = {
+    val administrativeArea = None
+    new Country(country.getString("CountryNameCode"), country.getString("CountryName"), administrativeArea)
+  }
+
+  def parseAccuracy(accuracy: Int) = {}
 
   def exception(msg: String) = new Exception(msg)
 }
@@ -36,6 +44,4 @@ private final class NullSafeJSONObject(source: JSONObject) {
   def jsonArray(key: String): Option[JSONArray] = onull(source.optJSONArray(key))
 
   def string(key: String): Option[String] = onull(source.optString(key))
-
-  def double(key: String): Option[Double] = onull(source.optDouble(key))
 }
--- src/main/scala/geoscala/google/types/Types.scala
+++ src/main/scala/geoscala/google/types/Types.scala
@@ -2,35 +2,58 @@ package geoscala.google.types
 
 final case class PostalCode(postalCodeNumber: String)
 final case class Thoroughfare(name: String)
-final case class Locality(name: String, thoroughfare: Thoroughfare, postalCode: PostalCode)
-final case class AdministrativeArea(name: String, locality: Locality)
-final case class Country(nameCode: String, name: String, administrativeArea: AdministrativeArea)
+final case class Locality(name: String, thoroughfare: Option[Thoroughfare], postalCode: Option[PostalCode])
+final case class AdministrativeArea(name: String, locality: Option[Locality])
+final case class Country(nameCode: String, name: String, administrativeArea: Option[AdministrativeArea])
 final case class AddressDetails(country: Country, accuracy: GeoLocationSearchAccuracy)
 final case class Placemark(identifier: String, address: String, details: AddressDetails)
 
 object GeoLocationSearchAccuracy {
   def unknown: GeoLocationSearchAccuracy = SearchAccuracyUnknown
+
   def country: GeoLocationSearchAccuracy = SearchAccuracyCountry
+
   def region: GeoLocationSearchAccuracy = SearchAccuracyRegion
+
   def subregion: GeoLocationSearchAccuracy = SearchAccuracySubRegion
+
   def town: GeoLocationSearchAccuracy = SearchAccuracyTown
+
   def postcode: GeoLocationSearchAccuracy = SearchAccuracyPostcode
+
   def street: GeoLocationSearchAccuracy = SearchAccuracyStreet
+
   def intersection: GeoLocationSearchAccuracy = SearchAccuracyIntersection
+
   def address: GeoLocationSearchAccuracy = SearchAccuracyAddress
+
   def premise: GeoLocationSearchAccuracy = SearchAccuracyPremise
+
+  implicit def intToGeoLocationSearchAccuracy(i: Int): GeoLocationSearchAccuracy = i match {
+    case 0 => unknown
+    case 1 => country
+    case 2 => region
+    case 3 => subregion
+    case 4 => town
+    case 5 => postcode
+    case 6 => street
+    case 7 => intersection
+    case 8 => address
+    case 9 => premise
+    case _ => error("Unknown accuracy: " + i)
+  }
 }
 
 sealed trait GeoLocationSearchAccuracy {
   def accuracy: Int
 }
-case object SearchAccuracyUnknown extends GeoLocationSearchAccuracy { def accuracy = 0 }
-case object SearchAccuracyCountry extends GeoLocationSearchAccuracy { def accuracy = 1 }
-case object SearchAccuracyRegion extends GeoLocationSearchAccuracy { def accuracy = 2 }
-case object SearchAccuracySubRegion extends GeoLocationSearchAccuracy { def accuracy = 3 }
-case object SearchAccuracyTown extends GeoLocationSearchAccuracy { def accuracy = 4 }
-case object SearchAccuracyPostcode extends GeoLocationSearchAccuracy { def accuracy = 5 }
-case object SearchAccuracyStreet extends GeoLocationSearchAccuracy { def accuracy = 6 }
-case object SearchAccuracyIntersection extends GeoLocationSearchAccuracy { def accuracy = 7 }
-case object SearchAccuracyAddress extends GeoLocationSearchAccuracy { def accuracy = 8 }
-case object SearchAccuracyPremise extends GeoLocationSearchAccuracy { def accuracy = 9 }
+case object SearchAccuracyUnknown extends GeoLocationSearchAccuracy {def accuracy = 0}
+case object SearchAccuracyCountry extends GeoLocationSearchAccuracy {def accuracy = 1}
+case object SearchAccuracyRegion extends GeoLocationSearchAccuracy {def accuracy = 2}
+case object SearchAccuracySubRegion extends GeoLocationSearchAccuracy {def accuracy = 3}
+case object SearchAccuracyTown extends GeoLocationSearchAccuracy {def accuracy = 4}
+case object SearchAccuracyPostcode extends GeoLocationSearchAccuracy {def accuracy = 5}
+case object SearchAccuracyStreet extends GeoLocationSearchAccuracy {def accuracy = 6}
+case object SearchAccuracyIntersection extends GeoLocationSearchAccuracy {def accuracy = 7}
+case object SearchAccuracyAddress extends GeoLocationSearchAccuracy {def accuracy = 8}
+case object SearchAccuracyPremise extends GeoLocationSearchAccuracy {def accuracy = 9}
